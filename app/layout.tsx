588bdd792251146341777b957a8d7444bc098911
vivekupasani <vivekupasani984@gmail.com>
2026-06-30 09:20:22 +0530

    fix: theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -146,7 +146,7 @@ export default async function RootLayout({
             <ConnectorsProvider>
               <ThemeProvider
                 attribute="class"
-                defaultTheme="system"
+                defaultTheme="light"
                 enableSystem
                 disableTransitionOnChange
                 themes={[
